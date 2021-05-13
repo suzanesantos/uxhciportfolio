8263ed8808063fde08bc4f45b67b85646c5720fe
suzanesantos <18030033@student.hhs.nl>
2021-05-13 23:31:29 +0200

    uxportfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,6 +51,7 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title', {})
+sr.reveal('.home__title__desc', {origin:'left', delay: 200})
 sr.reveal('.home__scroll', {delay: 200})
 sr.reveal('.home__img', {origin:'right', delay: 400})
 
